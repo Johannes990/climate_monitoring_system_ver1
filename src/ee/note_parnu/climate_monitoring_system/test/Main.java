e0c249242a86cc735fe71a46e3079755a17c74f6
Johannes <jojurg@ttu.ee>
2024-05-02 15:31:22 +0300

    reading xml from single file or from dir of files is pretty much ok now

--- src/ee/note_parnu/climate_monitoring_system/test/Main.java
+++ src/ee/note_parnu/climate_monitoring_system/test/Main.java
@@ -6,15 +6,22 @@ import ee.note_parnu.climate_monitoring_system.test.xml.parse.XMLParser;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
 public class Main {
-    public static void main(String[] args) throws ParserConfigurationException, SAXException {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
         final String xmlDir = "C:\\Users\\johan\\Git\\climate_monitoring_system_ver1\\src\\ee\\note_parnu\\climate_monitoring_system\\test\\test_scripts\\generated_xmls";
+        final String testXmlPath = "C:\\Users\\johan\\Git\\climate_monitoring_system_ver1\\src\\ee\\note_parnu\\climate_monitoring_system\\test\\test_scripts\\generated_xmls\\3510_15961202_0.xml";
+
         XMLParser xmlParser = new XMLParser();
+        XMLParser xmlParser1 = new XMLParser();
 
         ReadingsData xmlDirectoryData = xmlParser.parseDirectory(xmlDir);
+        xmlParser1.parseXml(testXmlPath);
+        ReadingsData singleXmlData = xmlParser1.getXMLData();
+        Reading singleReading = singleXmlData.getReadingList().get(0);
 
         assert (Objects.nonNull(xmlDirectoryData));
 
@@ -36,5 +43,16 @@ public class Main {
 
         }
 
+        System.out.println("Single xml data read");
+        System.out.println("pass key: " + singleReading.getPassKey());
+        System.out.println("temperature: " + singleReading.getTemp());
+        System.out.println("humidity: " + singleReading.getRelHum());
+        System.out.println("compQuant: " + singleReading.getCompQuant());
+        System.out.println("pressure: " + singleReading.getPressure());
+        System.out.println("alarms: " + singleReading.getAlarms());
+        System.out.println("compType: " + singleReading.getCompType());
+        System.out.println("tempU: " + singleReading.getTempU());
+        System.out.println("pressureU: " + singleReading.getPressureU());
+        System.out.println("timer: " + singleReading.getTimer());
     }
 }
