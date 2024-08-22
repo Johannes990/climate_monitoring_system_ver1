a6fde5680e248f909688361ca657d2c7146e42ed
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-23 01:02:05 +0300

    tra miks testid huiavad

--- src/test/java/com/climate_monitoring_system/repository/userauth/AccountStatusRepositoryTest.java
+++ src/test/java/com/climate_monitoring_system/repository/userauth/AccountStatusRepositoryTest.java
@@ -2,10 +2,13 @@ package com.climate_monitoring_system.repository.userauth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import com.climate_monitoring_system.repository.userauth.AccountStatusRepository;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @DataJpaTest
 public class AccountStatusRepositoryTest {
@@ -15,6 +18,6 @@ public class AccountStatusRepositoryTest {
 
     @Test
     public void testAccountStatusRepositoryNotNull() {
-        assertNotNull(accountStatusRepository, "Account status repository should not be null!");
+        assertThat(accountStatusRepository).isNotNull();
     }
 }
