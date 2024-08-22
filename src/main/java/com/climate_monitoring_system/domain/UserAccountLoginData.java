f782b2dfd6840afbb03018041f737ca5e6a65788
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-22 21:54:09 +0300

    creating entities

---
+++ src/main/java/com/climate_monitoring_system/domain/UserAccountLoginData.java
@@ -0,0 +1,35 @@
+package com.climate_monitoring_system.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "UserAccountLoginData", schema = "userauth")
+public class UserAccountLoginData {
+    @Id
+    @OneToOne
+    @JoinColumn(name = "UserID")
+    private UserAccountInfo userAccountInfo;
+
+    @Column(name = "Email")
+    @NotNull
+    @Email
+    private String email;
+
+    @Column(name = "PasswordHash")
+    @NotNull
+    private String passwordHash;
+
+    @Column(name = "PasswordSalt")
+    @NotNull
+    private String passwordSalt;
+
+    @JoinColumn(name = "HashAlgorithmID")
+    @ManyToOne
+    private HashAlgorithm hashAlgorithmID;
+}
