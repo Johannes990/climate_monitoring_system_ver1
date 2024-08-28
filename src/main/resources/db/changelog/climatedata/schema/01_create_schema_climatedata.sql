24ebc2683f579ab941052750b04245bc3fb20e5c
Johannes <jojurg@ttu.ee>
2024-08-28 11:49:08 +0300

    db files

---
+++ src/main/resources/db/changelog/climatedata/schema/01_create_schema_climatedata.sql
@@ -0,0 +1,5 @@
+--liquibase formatted sql
+
+--changeset JohannesJyrgenson20240828:1
+DROP SCHEMA IF EXISTS climatedata;
+CREATE SCHEMA climatedata;
---
+++ src/main/resources/db/changelog/userauth/schema/01_create_schema_userauth.sql
@@ -0,0 +1,5 @@
+--liquibase formatted sql
+
+--changeset JohannesJyrgenson20240828:1
+DROP SCHEMA IF EXISTS userauth;
+CREATE SCHEMA userauth;
