98f117b7c3ce139e5d07a3c13a4b6de74960032c
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-30 09:04:26 +0300

    permission stuff

--- src/test/java/com/climate_monitoring_system/repository/userauth/permissionRepositoryTest.java
+++ src/test/java/com/climate_monitoring_system/repository/userauth/permissionRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class permissionRepositoryTest {
@@ -16,11 +18,18 @@ public class permissionRepositoryTest {
     final String permissionName = "permissionName";
     final String permissionDescription = "permissionDescription";
 
-    public Permission permissionEntityGen(String statusName, String permissionStatus) {
+    public Permission permissionEntityGen(String permissionName, String permissionStatus) {
         Permission permission = new Permission();
+        permission.setPermissionName(permissionName);
+        permission.setPermissionDescription(permissionDescription);
+        return permission;
     }
 
-    public Permission saveAccountStatus(Permission permission) {
+    public Permission savePermission(Permission permission) {
         return permissionRepository.save(permission);
+    }
+
+    public void testPermissionRepositoryNotNull() {
+        assertThat(permissionRepository).isNotNull();
     }
 }
