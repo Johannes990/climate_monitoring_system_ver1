d6b3bf2fecbb72e87f98e3ee694c20f01837c1c0
Johannes <jojurg@ttu.ee>
2024-09-03 18:53:46 +0300

    security

--- src/main/java/com/climate_monitoring_system/ClimateMonitoringSystemApplication.java
+++ src/main/java/com/climate_monitoring_system/ClimateMonitoringSystemApplication.java
@@ -2,10 +2,7 @@ package com.climate_monitoring_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
