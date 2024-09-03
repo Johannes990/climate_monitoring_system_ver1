791d2b4d44341ba9d8908ebdd7e52734a32fdac0
Johannes <jojurg@ttu.ee>
2024-09-03 18:54:42 +0300

    registering in the frontside

---
+++ frontend/src/app/utils/api.ts
@@ -0,0 +1,25 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+
+export async function postRequest<T>(url: string, data: T): Promise<Response> {
+    try {
+        console.log(`${API_URL}${url}`)
+
+        const response = await fetch(`${API_URL}${url}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+
+        if (!response.ok) {
+            throw new Error("Network response was not OK.");
+        }
+
+        return response;
+    } catch (error) {
+        console.error("API post request error:", error);
+        throw error;
+    }
+}
