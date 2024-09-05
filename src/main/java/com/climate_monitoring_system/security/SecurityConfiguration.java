532cc50c626f35859d687ab2304bedf7274433b7
Johannes <jojurg@ttu.ee>
2024-09-05 18:27:13 +0300

    security working here

--- src/main/java/com/climate_monitoring_system/security/SecurityConfiguration.java
+++ src/main/java/com/climate_monitoring_system/security/SecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.climate_monitoring_system.security;
 
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfiguration {
@@ -12,17 +15,27 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable) // allow cors
                 .authorizeHttpRequests((authz) -> authz
                         .requestMatchers("/").permitAll()
                         .requestMatchers("/login").permitAll()
                         .requestMatchers("/register").permitAll()
                         .requestMatchers("/users").permitAll()
-                        .requestMatchers("/accounts").permitAll()
-                        .requestMatchers("/find/").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable);
         return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedHeader("*"); // to allow headers
+        config.addAllowedMethod("*"); // to allow post methods like GET, POST etc
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
