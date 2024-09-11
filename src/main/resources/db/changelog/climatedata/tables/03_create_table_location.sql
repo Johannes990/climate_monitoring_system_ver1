5d1d6950ffdf697ebb708b4b07e40edc98e33fcc
Johannes <jojurg@ttu.ee>
2024-09-11 17:06:39 +0300

    new changeset for climatedata.Location

--- src/main/resources/db/changelog/climatedata/tables/03_create_table_location.sql
+++ src/main/resources/db/changelog/climatedata/tables/03_create_table_location.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 --changeset JohannesJyrgenson20240911:1
-CREATE TABLE climatedata.Location (
+CREATE TABLE Location (
     LocationId INT IDENTITY(1, 1) NOT NULL,
     LocationDescription NVARCHAR(MAX),
     ControlParameterSetId INT,
@@ -10,4 +10,17 @@ CREATE TABLE climatedata.Location (
                       REFERENCES climatedata.ControlParameterSet(ControlParameterSetId)
                       ON UPDATE CASCADE
                       ON DELETE SET NULL
+);
+
+--changeset JohannesJyrgenson20240911:2
+DROP TABLE IF EXISTS Location;
+CREATE TABLE climatedata.Location (
+                          LocationId INT IDENTITY(1, 1) NOT NULL,
+                          LocationDescription NVARCHAR(MAX),
+                          ControlParameterSetId INT,
+                          CONSTRAINT PK_Location_LocationId PRIMARY KEY CLUSTERED (LocationId),
+                          CONSTRAINT FK_Location_ControlParameterSet FOREIGN KEY (ControlParameterSetId)
+                              REFERENCES climatedata.ControlParameterSet(ControlParameterSetId)
+                              ON UPDATE CASCADE
+                              ON DELETE SET NULL
 );
