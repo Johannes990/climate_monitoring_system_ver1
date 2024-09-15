ff791f2586648928403a5b583348dc8bca2e2978
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-09-15 17:22:21 +0300

    adding some readings to db, so we can test SensorReadingController

--- src/main/resources/db/changelog/climatedata/tables/05_create_table_sensorreading.sql
+++ src/main/resources/db/changelog/climatedata/tables/05_create_table_sensorreading.sql
@@ -37,3 +37,46 @@ CREATE TABLE climatedata.SensorReading (
                                            CONSTRAINT FK_SensorReading_Sensor FOREIGN KEY (SensorId)
                                                REFERENCES climatedata.Sensor(SensorId)
 );
+
+--changeset JohannesJyrgenson20240915:4
+INSERT INTO climatedata.SensorReading (Temperature, RelHumidity, SensorId)
+VALUES
+    (20.8, 48.0, 1),
+    (19.1, 40.3, 2);
+
+--changeset JohannesJyrgenson20240915:5
+INSERT INTO climatedata.SensorReading (Temperature, RelHumidity, SensorId)
+VALUES
+    (21.3, 47.6, 1),
+    (19.2, 40.7, 2),
+    (13.2, 20.1, 3),
+    (26.1, 44.0, 4);
+
+--changeset JohannesJyrgenson20240915:6
+INSERT INTO climatedata.SensorReading (Temperature, RelHumidity, SensorId)
+VALUES
+    (22.0, 50.1, 1),
+    (19.0, 40.1, 2),
+    (14.1, 28.3, 3),
+    (24.9, 43.0, 4);
+
+--changeset JohannesJyrgenson20240915:7
+INSERT INTO climatedata.SensorReading (Temperature, RelHumidity, SensorId)
+VALUES
+    (21.5, 52.1, 1),
+    (18.5, 39.9, 2),
+    (12.9, 31.1, 3),
+    (27.8, 47.1, 4);
+
+--changeset JohannesJyrgenson20240915:8
+INSERT INTO climatedata.SensorReading (Temperature, RelHumidity, SensorId)
+VALUES
+    (20.9, 49.3, 1),
+    (19.2, 40.1, 2),
+    (12.5, 26.7, 3),
+    (30.2, 51.2, 4);
+
+--changeset JohannesJyrgenson20240915:9
+INSERT INTO climatedata.SensorReading (Temperature, RelHumidity, SensorId)
+VALUES
+    (20.1, 39.8, 2);
