6db3ed434a15dc453088abcd650c8597fff9e7db
Johannes <jojurg@ttu.ee>
2024-10-18 11:51:44 +0300

    auth hell

--- src/main/java/com/climate_monitoring_system/security/SecurityConfiguration.java
+++ src/main/java/com/climate_monitoring_system/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.climate_monitoring_system.security;
 
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,12 +22,24 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())) // allow cors
+                .headers(headers -> headers
+                        .cacheControl(HeadersConfigurer.CacheControlConfig::disable))
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/", "/login", "/logout", "/register").permitAll()
+                        .requestMatchers("/login", "/logout", "/register").permitAll()
+                        .requestMatchers("/controlparams/all").authenticated()
+                        .requestMatchers("/locations/all").authenticated()
+                        .requestMatchers("/sensors/all").authenticated()
                         .anyRequest().authenticated()
                 )
                 .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID")
+                        .logoutSuccessUrl("/login")
+                );
         return http.build();
     }
 
