6d55b0c6bf05a8a34f931d2c17d53f44d47246c5
Johannes <jojurg@ttu.ee>
2024-10-25 11:32:53 +0300

    notification schema

--- src/main/resources/db/changelog/climatedata/tables/05_create_table_sensorreading.sql
+++ src/main/resources/db/changelog/climatedata/tables/05_create_table_sensorreading.sql
@@ -15,27 +15,27 @@ CREATE TABLE SensorReading (
 --changeset JohannesJyrgenson20240911:2
 DROP TABLE IF EXISTS SensorReading;
 CREATE TABLE climatedata.SensorReading (
-                                           SensorReadingId INT IDENTITY(1, 1) NOT NULL,
-                                           Temperature REAL,
-                                           RelHumidity REAL,
-                                           ReadingTime DATETIME,
-                                           SensorId INT NOT NULL,
-                                           CONSTRAINT PK_SensorReading_SensorReadingId PRIMARY KEY CLUSTERED(SensorReadingId),
-                                           CONSTRAINT FK_SensorReading_Sensor FOREIGN KEY (SensorId)
-                                               REFERENCES climatedata.Sensor(SensorId)
+    SensorReadingId INT IDENTITY(1, 1) NOT NULL,
+    Temperature REAL,
+    RelHumidity REAL,
+    ReadingTime DATETIME,
+    SensorId INT NOT NULL,
+    CONSTRAINT PK_SensorReading_SensorReadingId PRIMARY KEY CLUSTERED(SensorReadingId),
+    CONSTRAINT FK_SensorReading_Sensor FOREIGN KEY (SensorId)
+        REFERENCES climatedata.Sensor(SensorId)
 );
 
 --changeset JohannesJyrgenson20240912:3
 DROP TABLE IF EXISTS climatedata.SensorReading;
 CREATE TABLE climatedata.SensorReading (
-                                           SensorReadingId INT IDENTITY(1, 1) NOT NULL,
-                                           Temperature REAL,
-                                           RelHumidity REAL,
-                                           ReadingTime DATETIME DEFAULT CURRENT_TIMESTAMP,
-                                           SensorId INT NOT NULL,
-                                           CONSTRAINT PK_SensorReading_SensorReadingId PRIMARY KEY CLUSTERED(SensorReadingId),
-                                           CONSTRAINT FK_SensorReading_Sensor FOREIGN KEY (SensorId)
-                                               REFERENCES climatedata.Sensor(SensorId)
+    SensorReadingId INT IDENTITY(1, 1) NOT NULL,
+    Temperature REAL,
+    RelHumidity REAL,
+    ReadingTime DATETIME DEFAULT CURRENT_TIMESTAMP,
+    SensorId INT NOT NULL,
+    CONSTRAINT PK_SensorReading_SensorReadingId PRIMARY KEY CLUSTERED(SensorReadingId),
+    CONSTRAINT FK_SensorReading_Sensor FOREIGN KEY (SensorId)
+        REFERENCES climatedata.Sensor(SensorId)
 );
 
 --changeset JohannesJyrgenson20240915:4
