1549a1ee5c27d5dbe39f51e1f1b8edcf39698d74
Johannes <jojurg@ttu.ee>
2024-11-14 22:34:05 +0200

    active notifications on dashboard

--- frontend/src/app/dashboard/dashboardMain/page.tsx
+++ frontend/src/app/dashboard/dashboardMain/page.tsx
@@ -1,17 +1,32 @@
 "use client";
 
 import { getRequest } from "@/app/utils/api";
-import { useEffect, useState } from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import { useRouter } from "next/navigation";
 import {
     LOGIN_URL_PATH,
     PROTECTED_QUERY_PATH,
 } from "@/app/utils/constants";
+import {NotificationDTO} from "@/app/dto/notification/NotificationDTO";
+import {fetchActiveNotifications, fetchAllNotifications} from "@/app/dashboard/notifications/notificationService";
+import NotificationTable from "@/app/dashboard/components/NotificationTable";
 
 export default function Dashboard() {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
     const [errorMessage, setErrorMessage] = useState<string | null>(null);
+    const [activeNotifications, setActiveNotifications] = useState<NotificationDTO[]>([]);
+    const [error, setError] = useState<string>("");
     const router = useRouter();
+
+    const fetchData = useCallback(async () => {
+        try {
+            const activeNotifications = await fetchActiveNotifications(true);
+            setActiveNotifications(activeNotifications);
+        } catch (err) {
+            setError("Failed to fetch all notifications");
+            console.error(err);
+        }
+    }, []);
 
     useEffect(() => {
         window.history.replaceState(null, "", window.location.href);
@@ -43,7 +58,8 @@ export default function Dashboard() {
         };
 
         checkAuthentication();
-    }, [router]);
+        fetchData();
+    }, [fetchData]);
 
     if (errorMessage) {
         return (
@@ -68,9 +84,12 @@ export default function Dashboard() {
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-6 bg-gray-100">
-            <div className="w-full max-w-md p-8 space-y-6 bg-white rounded shadow-md">
+            <div className="w-full max-w-4xl p-8 space-y-6 bg-white rounded shadow-md">
                 <h1 className="text-3xl font-bold text-center">Dashboard</h1>
                 <p className="text-center">Welcome to your dashboard!</p>
+
+                <h1 className="text-3xl font-bold text-center">Active Notifications</h1>
+                <NotificationTable notifications={activeNotifications}/>
             </div>
         </main>
     );
